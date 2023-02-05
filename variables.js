4b4fb6e67f4826826ed85447567c80e5fe532df1
Kristin Schlossman <Kristin.Schlossman@gmail.com>
2023-02-04 17:35:27 -0800

    Add command for printing boolean expressions.

--- variables.js
+++ variables.js
@@ -1,3 +1,18 @@
  var debtIsPaid = false
+ debtIsPaid // => false
  var drankWater = true
+ drankWater // => true
  var fewLinesMade = true 
+fewLinesMade // => true
+
+
+debtIsPaid = true
+debtIsPaid // => true
+drankWater = false 
+drankWater // => false
+fewLinesMade = false 
+fewLinesMade // => false 
+
+console.log(debtIsPaid)
+console.log(drankWater)
+console.log(fewLinesMade)
